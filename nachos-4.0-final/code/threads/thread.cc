d516de26f42d2f778dfc745221e11602a1df3a84
louis208908 <lewis8908@gmail.com>
2022-05-29 22:45:46 +0800

    fix typo

--- nachos-4.0-final/code/threads/thread.cc
+++ nachos-4.0-final/code/threads/thread.cc
@@ -296,7 +296,7 @@ Thread::Sleep (bool finishing)
                         << nextThread->getID( )
                         << "] is now selected for execution, thread ["
                         << this->getID( )
-                        << "] us replaced, and it has executed ["
+                        << "] is replaced, and it has executed ["
                         << this->getBurstTime( ) << "] ticks");
     // returns when it's time for us to run
     
